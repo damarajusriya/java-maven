429e73a71f2a410be2d229cf6e793b4c382c9f91
Gaurav Shankar <gaurav.shankar@freshworks.com>
2020-04-27 17:43:33 +0530

    fixing project run

--- src/main/java/com/hexa/App.java
+++ src/main/java/com/hexa/App.java
@@ -1,4 +1,5 @@
 package com.hexa;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -6,45 +7,41 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-/**
-* Hello world!
-*
-*/
-public class App
-{
- public static void main( String[] args )
- {
- StandardServiceRegistry ssr=new
-StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
- Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+import org.hibernate.cfg.Configuration;
 
- SessionFactory factory=meta.getSessionFactoryBuilder().build();
- 
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+
 //Configuration().configure().buildSessionFactory(); //This line to be replaced with
 //below commented line
- Session session=factory.openSession();
+        Session session = sessionFactory.openSession();
 
- Transaction t=session.beginTransaction();
+        Transaction t = session.beginTransaction();
 
- Employee e1=new Employee();
- e1.setName("Gaurav Chawla");
+        Employee e1 = new Employee();
+        e1.setName("Gaurav Chawla");
 
- PerEmployee e2=new PerEmployee();
- e2.setName("Vivek Kumar");
- e2.setSalary(50000);
- e2.setBonus(5);
+        PerEmployee e2 = new PerEmployee();
+        e2.setName("Vivek Kumar");
+        e2.setSalary(50000);
+        e2.setBonus(5);
 
- ContEmployee e3=new ContEmployee();
- e3.setName("Arjun Kumar");
- e3.setPay_per_hour(1000);
- e3.setContract_duration("15 hours");
+        ContEmployee e3 = new ContEmployee();
+        e3.setName("Arjun Kumar");
+        e3.setPay_per_hour(1000);
+        e3.setContract_duration("15 hours");
 
- session.persist(e1);
- session.persist(e2);
- session.persist(e3);
+        session.persist(e1);
+        session.persist(e2);
+        session.persist(e3);
 
- t.commit();
- session.close();
- System.out.println("success");
- }
-} 
+        t.commit();
+        session.close();
+        System.out.println("success");
+    }
+}
--- src/main/java/com/hexa/Employee.java
+++ src/main/java/com/hexa/Employee.java
@@ -1,4 +1,5 @@
 package com.hexa;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,39 +8,47 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "employee")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Employee {
-@Id
-@GeneratedValue(strategy=GenerationType.AUTO)
-@Column(name = "id")
-private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
 
-@Column(name = "name")
-private String name;
+    @Column(name = "name")
+    private String name;
 
-public Employee(int id, String name) {
-super();
-this.id = id;
-this.name = name;
-}
-@Override
-public String toString() {
-return "Employee [id=" + id + ", name=" + name + "]";
-}
-public void setId(int id) {
-this.id = id;
-}
-public void setName(String name) {
-this.name = name;
-}
-public int getId() {
-return id;
-}
-public String getName() {
-return name;
-}
-public Employee() {
+    public Employee(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + "]";
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee() {
 // TODO Auto-generated constructor stub
-} }
+    }
+}
